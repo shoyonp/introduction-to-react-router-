b2ce65007d70a6afcabddd1a636f32e0dfb23d2e
shoyonp <shoyonkumar143@gmail.com>
2024-10-29 01:10:14 +0600

    loader and use loader

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,12 +1,28 @@
+import { useLoaderData } from "react-router-dom";
+import User from "../User/User";
+import './Users.css'
+
 const Users = () => {
-    return (
-        <div>
-            <div>
-                <h2>Our Users</h2>
-                <p>fantastic and  awesome</p>
-            </div>
+  const users = useLoaderData();
+
+  // state --> data
+  // state --> loader
+  // us effect
+  // fetch --> state set --> set state
+  console.log(users);
+  return (
+    <div>
+      <div>
+        <h2>Our Users: {users.length}</h2>
+        <p>fantastic and awesome</p>
+        <div className="users">
+          {users.map((user) => (
+            <User key={user.id} user={user}></User>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Users;
--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Home from './components/Home/Home.jsx';
-import About from './components/About/About.jsx';
-import Contact from './components/Contact/Contact.jsx';
-import Users from './components/Users/Users.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import Users from "./components/Users/Users.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Home></Home>,
     children: [
       {
-        path: '/about',
-        element: <About></About>
+        path: "/about",
+        element: <About></About>,
       },
       {
-        path: '/contact',
-        element: <Contact></Contact>
+        path: "/contact",
+        element: <Contact></Contact>,
       },
       {
-        path:  '/users',
-        element: <Users></Users>
-      }
-    ]
+        path: "/users",
+        loader: () => fetch("https://jsonplaceholder.typicode.com/users"),
+        element: <Users></Users>,
+      },
+    ],
   },
   // {
   //   path: '/about',
@@ -38,11 +36,10 @@ const router = createBrowserRouter([
   //   path: '/contact',
   //   element: <div>Call me right now!!</div>
   // }
-  
-])
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router}></RouterProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
